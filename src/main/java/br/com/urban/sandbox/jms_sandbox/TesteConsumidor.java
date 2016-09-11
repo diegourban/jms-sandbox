40efe0078149a5600c1c4579afd2a718981dcb61
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-09-11 16:50:12 -0300

    receiving messages with messagelistener

--- src/main/java/br/com/urban/sandbox/jms_sandbox/TesteConsumidor.java
+++ src/main/java/br/com/urban/sandbox/jms_sandbox/TesteConsumidor.java
@@ -1,11 +1,16 @@
 package br.com.urban.sandbox.jms_sandbox;
 
+import java.util.Scanner;
+
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
 import javax.jms.Destination;
+import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageConsumer;
+import javax.jms.MessageListener;
 import javax.jms.Session;
+import javax.jms.TextMessage;
 import javax.naming.InitialContext;
 
 public class TesteConsumidor {
@@ -21,8 +26,20 @@ public class TesteConsumidor {
 		Destination fila = (Destination) context.lookup("financeiro");
 		MessageConsumer consumer = session.createConsumer(fila);
 		
-		Message message = consumer.receive();
-		System.out.println("Receiving message: " + message);
+		consumer.setMessageListener(new MessageListener() {
+			
+			@Override
+			public void onMessage(Message message) {
+				TextMessage textMessage = (TextMessage) message;
+				try {
+					System.out.println("Receiving message: " + textMessage.getText());
+				} catch (JMSException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		
+		new Scanner(System.in).nextLine();
 		
 		session.close();
 		connection.close();
